1657fddcec5a6791a29358d1c82c17a32da348e9
nksound <NKSound@mail.ru>
2024-12-22 14:22:27 +0300

    "CRUD Запросы: Get, Post, Put Delete."

--- module_16_3.py
+++ module_16_3.py
@@ -1,16 +1,9 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import Dict
-
+from fastapi import FastAPI, Path
+from typing import Dict, Annotated
 
 app = FastAPI()
 
 users: Dict[str, str] = {'1': 'Имя: Example, возраст: 18'}
-
-
-class User(BaseModel):
-    username: str
-    age: int
 
 
 @app.get("/users")
@@ -19,19 +12,34 @@ async def get_users():
 
 
 @app.post("/user/{username}/{age}")
-async def post_user(username: str, age: int):
+async def post_user(
+        username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example="UrbanUser")],
+        age: Annotated[int, Path(ge=18, le=120, description="Enter age", example=24)]
+):
     user_id = str(max(map(int, users.keys())) + 1)
     users[user_id] = f"Имя: {username}, возраст: {age}"
     return f"User {user_id} is registered"
 
 
 @app.put("/user/{user_id}/{username}/{age}")
-async def update_user(user_id: str, username: str, age: int):
+async def update_user(
+        user_id: Annotated[str, Path(regex="^[1-9][0-9]*$", description="Enter User ID", example="1")],
+        username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example="UrbanProfi")],
+        age: Annotated[int, Path(ge=18, le=120, description="Enter age", example=28)]
+):
     users[user_id] = f"Имя: {username}, возраст: {age}"
     return f"The user {user_id} is updated"
 
 
 @app.delete("/user/{user_id}")
-async def delete_user(user_id: str):
+async def delete_user(
+        user_id: Annotated[str, Path(regex="^[1-9][0-9]*$", description="Enter User ID", example="1")]
+):
+    del users[user_id]
+    return f"User {user_id} has been deleted"
+
+
+@app.delete("/user/{user_id}")
+async def delete_user(user_id: Annotated[str, Path(description="Enter User ID", example="1")]):
     del users[user_id]
     return f"User {user_id} has been deleted"
