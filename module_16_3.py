4dcd060f5f21c29fd5f8e05438679ac160844aca
nksound <NKSound@mail.ru>
2024-12-23 15:18:26 +0300

    "CRUD Запросы: Get, Post, Put Delete."

--- module_16_3.py
+++ module_16_3.py
@@ -16,9 +16,9 @@ async def post_user(
         username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example="UrbanUser")],
         age: Annotated[int, Path(ge=18, le=120, description="Enter age", example=24)]
 ):
-    user_id = str(max(map(int, users.keys())) + 1)
+    user_id = str(len(users) + 1)
     users[user_id] = f"Имя: {username}, возраст: {age}"
-    return f"User {user_id} is registered"
+    return {"message": f"User {user_id} is registered"}
 
 
 @app.put("/user/{user_id}/{username}/{age}")
@@ -27,19 +27,19 @@ async def update_user(
         username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example="UrbanProfi")],
         age: Annotated[int, Path(ge=18, le=120, description="Enter age", example=28)]
 ):
+    if user_id not in users:
+        return {"error": "User not found."}
+
     users[user_id] = f"Имя: {username}, возраст: {age}"
-    return f"The user {user_id} is updated"
+    return {"message": f"The user {user_id} is updated"}
 
 
 @app.delete("/user/{user_id}")
 async def delete_user(
         user_id: Annotated[str, Path(regex="^[1-9][0-9]*$", description="Enter User ID", example="1")]
 ):
-    del users[user_id]
-    return f"User {user_id} has been deleted"
+    if user_id not in users:
+        return {"error": "User not found."}
 
-
-@app.delete("/user/{user_id}")
-async def delete_user(user_id: Annotated[str, Path(description="Enter User ID", example="1")]):
     del users[user_id]
-    return f"User {user_id} has been deleted"
+    return {"message": f"User {user_id} has been deleted"}
